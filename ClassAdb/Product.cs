54e4682174aca8044697199514bed1fa90b1f368
Insori <s2110@e-mirim.hs.kr>
2023-05-25 14:27:40 +0900

    초기 커밋.

---
+++ ClassAdb/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAdb
+{
+    internal class Product
+    {
+        private string name;
+        private int price;
+
+        public Product(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        ~Product() 
+        {
+            Console.WriteLine(this.name + "의 소멸자 호출");
+        }
+    }
+}
---
+++ ClassAdb/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAdb
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //소멸자 실습
+            Product product1 = new Product("탕후루", 3000);
+            Product product2 = new Product("핫도그", 2000);
+            Product product3 = new Product("이클립스", 3000);
+            Console.WriteLine("안녕하세요.");
+        }
+    }
+}
